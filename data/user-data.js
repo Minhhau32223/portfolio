260d266ed29754713f80c9cbc90c2d93c250e0d0
Minhhau32223 <hau334779@gmail.com>
2025-02-25 22:14:23 +0700

    first commit

---
+++ data/user-data.js
@@ -0,0 +1,13 @@
+export const userData = {
+  githubUser: 'minhhau32223',
+  devUsername: "minhhau32223",
+  github: 'https://github.com/minhhau32223',
+  facebook: 'https://www.facebook.com/nguyen.minh.hau.751496',
+  linkedIn: 'https://www.linkedin.com/in/hau-nguyen-36b72a332/',
+  twitter: 'https://x.com/hau860828',
+  stackOverflow: 'https://stackoverflow.com/users/29795644/hau-nguyen',
+  leetcode: "https://leetcode.com/u/vENMftlEpd/",
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing",
+  skills: ['React', 'ASP.net',  'Express', 'NestJS', 'MySql', 'SQL Server', 'Postgres', 'Django', 'AWS'],
+  timezone: '+7'
+};
